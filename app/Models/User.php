da57065810a5c2650a343c7ce573520eb7e66cce
Mst.Shorifa <shorifaakter825@gmail.com>
2024-10-13 20:29:45 +0600

    model migration created

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,45 +4,49 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+class User extends Authenticatable {
+    use HasFactory, Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = [ 'name', 'email', 'password', 'role' ];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = [ 'password', 'remember_token' ];
 
     /**
-     * Get the attributes that should be cast.
+     * The attributes that should be cast.
      *
-     * @return array<string, string>
+     * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+        'role' => 'string',
+        'deleted_at' => 'datetime',
+    ];
+
+    public function response(): HasMany {
+        return $this->hasMany(Ticket::class, 'admin_id', 'id')->withTranshed();
+    }
+
+    public function creator(): HasMany {
+        return $this->hasMany(Ticket::class, 'customer_id', 'id')->withTranshed();
+    }
+
+    public function messages(): HasMany {
+        return $this->hasMany(Message::class, 'user_id', 'id')->withTranshed();
     }
 }
